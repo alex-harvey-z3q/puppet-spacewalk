08e475b47cfbae03ff1a50172f271906ee72fbc2
Alex Harvey <alex@razorconsulting.com.au>
2016-05-31 01:28:35 +1000

    Adds tests for answers file content

--- spec/classes/spacewalk_server_spec.rb
+++ spec/classes/spacewalk_server_spec.rb
@@ -11,4 +11,31 @@ describe 'spacewalk::server' do
     }}
     it { is_expected.to compile.with_all_deps }
   end
+
+  context 'answers file' do
+    [
+      [:admin_email,         'root@localhost'],
+      [:ssl_set_org,         'some org'      ],
+      [:ssl_set_org_unit,    'some org unit' ],
+      [:ssl_set_city,        'some city'     ],
+      [:ssl_set_state,       'some state'    ],
+      [:ssl_set_country,     'some country'  ],
+      [:ssl_password,        'sslpassword'   ],
+      [:ssl_set_email,       'root@localhost'],
+      [:ssl_config_sslvhost, 'Y'             ],
+      [:db_password,         'dbpassword'    ],
+      [:db_port,             '1234'          ],
+    ]
+    .each do |param, value|
+      context param do
+        let(:params) {{
+          param => value
+        }}
+        it {
+          is_expected.to contain_file('/etc/sysconfig/spacewalk.answers').
+            with_content(/#{value}/)
+        }
+      end
+    end
+  end
 end
