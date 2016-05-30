c3f7d2cf7baece32cb236fd85e07385c66ad695f
Duncan Webbe <duncanwebbe@gmail.com>
2016-05-30 23:09:30 +1000

    Modify answer file to use default postgresql port. Also modify default country parameter.

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,12 +18,14 @@ class spacewalk::params {
   $admin_email         = 'root@localhost'
   $ssl_set_org         = 'Spacewalk Org'
   $ssl_set_org_unit    = 'spacewalk'
+  $ssl_set_city        = 'My City'
   $ssl_set_state       = 'My State'
-  $ssl_set_country     = 'My Country'
+  $ssl_set_country     = 'AU'
   $ssl_password        = 'spacewalk'
   $ssl_set_email       = 'root@localhost'
   $ssl_config_sslvhost = 'Y'
   $db_password         = 'spacewalk'
+  $db_port	       = '5432'
 
   $yumrepos = {
     'spacewalk' => {
